20d2e6262d857484088512640330005ac6bf865e
suhaib079 <suhaibabdennabi33@gmail.com>
2021-07-14 21:57:16 +0300

    g

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+
+class movies_crud_test(TestCase):
+
+    def setUp(self):
+
+
+        self.user = get_user_model().objects.create_user(
+            username = 'suhaib',
+            email = 'suhaib33@gmail.com',
+            password = 'password'
+        )
+
+    def test_create_new_user(self):
+
+        self.assertEquals(self.user.email,'suhaib33@gmail.com')
+        self.assertEquals(self.user.username,'suhaib')
+        
+
+    def test_duplicate_emails(self):
+        try:
+
+            self.user2 = get_user_model().objects.create_user(
+                username = 'suhaib',
+                email = 'suhaib33@gmail.com',
+                password = 'password'
+            )
+            
+        except:
+            print('email already in use')
