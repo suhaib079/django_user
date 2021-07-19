9119eab8303c520cc6645833526797d1ce4eafbc
suhaib079 <suhaibabdennabi33@gmail.com>
2021-07-19 13:35:31 +0300

    f

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
-    # add additional fields in here
-
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(('email adress'), unique=True)
+    REQUIRED_FIELDS = ['username']
     def __str__(self):
-        return self.username
+        return self.email
